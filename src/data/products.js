b441dd807c1ead433aa2114034c0c1583ee36467
German Reynaga <german.reynagaa@gmail.com>
2019-12-15 17:07:04 -0600

    Add products data

---
+++ src/data/products.js
@@ -0,0 +1,27 @@
+const products = {
+  "new": [
+    {
+      "title": "Lizard",
+      "image_url": "https://images.unsplash.com/photo-1504450874802-0ba2bcd9b5ae?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1100&q=80",
+      "price": "17.40",
+      "description": "Lizards are a widespread group of squamate reptiles"
+    },
+    {
+      "title": "Tomatoes",
+      "image_url": "https://www.abelandcole.co.uk/media/7344_15922_z.jpg",
+      "price": "20.70"
+    },
+    {
+      "title": "Lizard",
+      "image_url": "https://images.unsplash.com/photo-1504450874802-0ba2bcd9b5ae?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1100&q=80",
+      "price": "17.40"
+    },
+    {
+      "title": "Lizard",
+      "image_url": "https://images.unsplash.com/photo-1504450874802-0ba2bcd9b5ae?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1100&q=80",
+      "price": "17.40"
+    }
+  ]
+}
+
+export default products
