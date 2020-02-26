c61e472f8775e7dbfb3a0e991998ca10efde1203
German Reynaga <german.reynagaa@gmail.com>
2020-02-26 03:39:20 -0600

    Fix UI errors with responsive

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -9,24 +9,35 @@ import {
 } from '@material-ui/core'
 import { connect } from 'react-redux'
 import { PaymentBox, CartItem } from '../components/Index'
+import { useGeneralClasses } from '../styles/index.js'
+import { LocalMall } from '@material-ui/icons'
 
 function Cart (props) {
   const classes = useStyles()
   const cartItems = props.cartItems
   const subTotalPrice = props.cartItems.reduce((sum, value) => sum + (value.price * value.count), 0)
   const totalPrice = subTotalPrice + (subTotalPrice * 0.16)
+  const generalClasses = useGeneralClasses()
 
   return (
     <div className={classes.wrapper}>
-      <Card>
+      <Card className={classes.defaultCartStyle}>
         <Grid container>
           <Grid item xs={12} sm={12} md={8}>
             <CardContent>
               <Typography variant='h5'>Carrito de compra</Typography>
               <List>
-                {cartItems.map(item => (
-                  <CartItem key={item.index} item={item} />
-                ))}
+                {cartItems.length > 0
+                  ? cartItems.map(item => (
+                    <CartItem key={item.index} item={item} />
+                  )) : <Grid container justify='center' className={generalClasses.margin_t_bg}>
+                    <Grid item xs={12}>
+                      <p style={{textAlign: 'center'}}>No items selected yet</p>
+                    </Grid>
+                    <Grid>
+                      <LocalMall className={`${classes.noItemsIcon}`} />
+                    </Grid>
+                  </Grid>}
               </List>
             </CardContent>
           </Grid>
@@ -45,6 +56,13 @@ const useStyles = makeStyles(_theme => ({
     justifyContent: 'center',
     marginTop: 50,
     flexGrow: 1
+  },
+  noItemsIcon: {
+    fontSize: 50,
+    color: '#3f51b5'
+  },
+  defaultCartStyle: {
+    width: '80%'
   }
 }))
 
